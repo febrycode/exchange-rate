83e5e8d716e9f1a41dda43376b68afbca72a976c
Febryan Setiawan <setiawanfebryan@gmail.com>
2018-08-28 14:17:56 +0700

    Implement create exchange rate

---
+++ app/controllers/daily_exchange_rates_controller.rb
@@ -0,0 +1,32 @@
+class DailyExchangeRatesController < ApplicationController
+
+  def create
+    exchange_code = ExchangeCode.find_by(
+      code_from: ex_rate_params["code_from"],
+      code_to: ex_rate_params["code_to"]
+    )
+
+    exchange_rate = DailyExchangeRate.new(
+      exchange_code_id: exchange_code.id,
+      date_input: ex_rate_params["date_input"],
+      rate_value: ex_rate_params["rate_value"]
+    )
+
+    if exchange_rate.save
+      render json: { message: "Data successfully created", error: false }, status: :created
+    else
+      render json: { message: "Data failed", error: true }, status: :ok
+    end
+  end
+
+  private
+
+  def ex_rate_params
+    params.require(:daily_exchange_rate).permit(
+      :code_from,
+      :code_to,
+      :date_input,
+      :rate_value
+    )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :exchange_codes, only: [:index, :create, :destroy]
+  resources :daily_exchange_rates, only: [:create]
 end
